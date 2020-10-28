39b4d816a90f1e9cae43d7fed435b44037fede56
rkmissile <rkmissile@rocketmail.com>
2020-10-28 15:07:18 -0400

    array..

---
+++ src/SDET/Array/SumArray.java
@@ -0,0 +1,15 @@
+package SDET;
+
+public class SumArray {
+    public static void main(String[] args) {
+        int a[]={2,3,4,5,6,7,8,9};
+        
+        int sum = 0;
+        
+        for(int i:a){
+            sum=sum+i;
+        }
+
+        System.out.println(sum);
+     }
+}
