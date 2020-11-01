1d95e350d400a6f7156aa4f94e63fc75df5818dc
rkmissile <rkmissile@rocketmail.com>
2020-11-01 13:31:39 -0500

    11/1

---
+++ src/day5/classwork/Execution.java
@@ -0,0 +1,10 @@
+package day5.classwork;
+
+public class Execution {
+
+    // create a interface with some business concept
+    // create a abstract class with some concept and some implementation
+    // create your business with those previous ideas
+    // and run your business and show how the methods are working in the Execution Class
+
+}
